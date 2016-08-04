b15ba77340e1edf778678389d4ec1775876bb003
KunAn Hsu <annbigbig@gmail.com>
2016-08-04 23:24:39 +0800

    init commit

---
+++ src/main/java/com/kashu/Test002gApplication.java
@@ -0,0 +1,12 @@
+package com.kashu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test002gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test002gApplication.class, args);
+	}
+}
